d9a1babaa21f44054d9699a87b370a5a93a81722
Nuttawad.nn <58647614+55nattawad@users.noreply.github.com>
2023-09-12 16:56:09 +0700

    Update MoneySimulatorZ.lua

--- MoneySimulatorZ.lua
+++ MoneySimulatorZ.lua
@@ -91,43 +91,21 @@ Section:NewToggle("Auto Upgrade Slot3", "Click To Upgrade", function(S3)
     }
 
     game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
+        end
+    end)
+
+Section:NewToggle("Auto Upgrade Slot4", "Click To Upgrade", function(S4)
+    _G.UpgradeSlot4 = S4
+    while _G.UpgradeSlot3 do wait ()
     local args = {
         [1] = 4,
         [2] = 1,
         [3] = false
     }
 
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("UpgradeMachine"):FireServer(unpack(args))
     local args = {
-        [1] = 5,
-        [2] = 1,
-        [3] = false
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
-    local args = {
-        [1] = 6,
-        [2] = 1,
-        [3] = false
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
-    local args = {
-        [1] = 7,
-        [2] = 1,
-        [3] = false
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
-    local args = {
-        [1] = 8,
-        [2] = 1,
-        [3] = false
-    }
-
-    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMoreMachines"):FireServer(unpack(args))
-    local args = {
-        [1] = 9,
+        [1] = 4,
         [2] = 1,
         [3] = false
     }
@@ -156,6 +134,42 @@ Section:NewToggle("Auto BuyMachine", "Click To BuyMachine", function(B1)
     game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
             local args = {
         [1] = 3,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 4,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 5,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 6,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 7,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 8,
+        [2] = false
+    }
+
+    game:GetService("ReplicatedStorage"):WaitForChild("Events"):WaitForChild("BuyMachine"):FireServer(unpack(args))
+            local args = {
+        [1] = 9,
         [2] = false
     }
 
